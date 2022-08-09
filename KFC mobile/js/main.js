7b3b05c54b23d1ffbcb6b99bebff8dadd8c8a8fa
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-08-09 18:18:48 +0200

    hamburger menu

--- KFC mobile/js/main.js
+++ KFC mobile/js/main.js
@@ -1,1 +1,19 @@
+const hamburgerHover = document.querySelector('.hamburgerHover')
+const hamburger = document.querySelector('#hamburger')
+const hamburger1 = document.querySelector('.hamburger1')
+const hamburger2 = document.querySelector('.hamburger2')
+const navbar = document.querySelector('.navbar')
+const logo = document.querySelector('#logo')
+hamburger.addEventListener('click', showMenu)
 
+function showMenu(){
+    if(hamburgerHover.style.display == 'flex'){
+        hamburgerHover.style.display = 'none'
+        navbar.style.flexDirection = 'row'
+    } else {
+    hamburgerHover.style.display = 'flex'
+    navbar.style.flexDirection = 'column'
+    navbar.style.margin = '0 auto'
+    
+            }
+}
